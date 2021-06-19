8fc09f83d1d71724b3e1965fd7c1850d744a17b8
Harsh <harshgupta1249@gmail.com>
2021-06-19 23:09:02 +0530

    Added about route

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { verifyToken } = require('../middlewares/tokenVerifier');
-const { userData, updateEmail, updatePassword, updateName } = require('../controller/profile');
+const { userData, updateEmail, updatePassword, updateName, updateAbout } = require('../controller/profile');
 const router = express.Router();
 
 router.post('/getUserData', verifyToken, userData);
 router.put('/updateEmail', verifyToken, updateEmail);
 router.put('/updateName', verifyToken, updateName);
 router.put('/updatePassword', verifyToken, updatePassword);
+router.put('/updateAbout', verifyToken, updateAbout);
 
 module.exports = router;
