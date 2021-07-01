dcf0952f146382f96374bb853e814331a59d369a
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-01 17:37:09 +0530

    Fixed Bug

--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ const { verifyToken } = require('../middlewares/tokenVerifier');
 const { userData, updateEmail, updatePassword, updateName, updateAbout, userInfo } = require('../controller/profile');
 const router = express.Router();
 
-router.post('/getUserData', verifyToken, userData);
+// router.post('/getUserData', verifyToken, userData);
 router.post('/getUserinfo', verifyToken, userInfo);
 router.put('/updateEmail', verifyToken, updateEmail);
 router.put('/updateName', verifyToken, updateName);
